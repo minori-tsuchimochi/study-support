58173b03fdb963b53bef821b05d00063d5834f5a
MinoriDMM <189940358+minori-tuchimoti@users.noreply.github.com>
2025-11-17 11:38:38 +0900

    初回コミット

---
+++ src/main/java/com/example/study_support/StudySupportApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study_support/StudySupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
